262167d94fa726d59828d76f545baf1fe06d4396
ipipip1735 <ipipip1735@163.com>
2018-10-05 10:00:54 +0800

    Update String

--- Lang/src/StringTrial.java
+++ Lang/src/StringTrial.java
@@ -1,10 +1,62 @@
+import java.util.Arrays;
+
 public class StringTrial {
     public static void main(String[] args) {
         StringTrial stringTrial = new StringTrial();
-        stringTrial.appendBool();
-        stringTrial.appendChars();
-        stringTrial.split();
-        stringTrial.compare();
+//        stringTrial.appendBool();
+//        stringTrial.appendChars();
+        stringTrial.join();
+//        stringTrial.split();
+//        stringTrial.compare();
+//        stringTrial.equal();
+//        stringTrial.base();
+
+
+
+    }
+
+    private void join() {
+        String message = String.join("-", "Java", "is", "cool");
+        System.out.println(message);
+    }
+
+    private void base() {
+        System.out.println(" ss ".trim());
+
+    }
+
+
+    private void equal() {
+
+        boolean b;
+        b= ("sss" == "sss");
+//        System.out.println("sss".hashCode());
+//        System.out.println("stt".hashCode());
+
+//        String s = "sss";
+//        b = (s == "sss");
+//
+//        String sk = "oo";
+//        String st = "oo";
+//        System.out.println(sk.hashCode());
+//        System.out.println(st.hashCode());
+//        b = (sk == st);
+
+
+//        String k = "sdf";
+//        String t = "sdf";
+//        System.out.println(t.hashCode());
+//        System.out.println(k.hashCode());
+//        b=k.equals(t);
+
+        String k = new String("sdf");
+        String t = new String("sdf");
+
+        System.out.println(b);
+
+
+
+
     }
 
 
@@ -15,13 +67,31 @@ public class StringTrial {
     }
 
     private static void split() {
-        String[] strings = null;
-        String s = "a/b/c/d";
-        strings = s.split("/");
-        for (String v :
-                strings) {
-            System.out.println(v);
-        }
+//        String[] strings = null;
+//        String s = "a/b/c/d";
+//        strings = s.split("/");
+//        for (String v :
+//                strings) {
+//            System.out.println(v);
+//        }
+
+        Arrays.stream("aa b1b       cc".split(" ")) //正则表达式没有+
+                .forEach(System.out::println);
+
+        Arrays.stream("aa bb       cc".split(" +"))
+                .forEach(System.out::println);
+
+        Arrays.stream("a1b2c3".split("[1-9]"))
+                .forEach(System.out::println);
+
+        Arrays.stream("a[3".split("\\["))  //匹配元字符时需要转义，使用\\
+                .forEach(System.out::println);//输出 a 3
+
+
+
+
+
+
     }
 
     private static void appendChars() {
