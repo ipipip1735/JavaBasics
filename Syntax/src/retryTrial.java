f42b441d9f58db01b8abb76a6a00afda3026243f
ipipip1735 <ipipip1735@163.com>
2018-10-26 17:35:57 +0800

    Update Lock & BlockingQueue

---
+++ Syntax/src/retryTrial.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Administrator on 2018/10/26 11:08.
+ */
+
+public class retryTrial {
+    public static void main(String[] args) {
+
+
+        retry:
+        for (int i = 10; i < 15; i++) {
+            System.out.println(i);
+
+
+            for (int j = 20; j < 25; j++) {
+                System.out.println(j);
+
+                for (int k = 30; k < 35; k++) {
+                    System.out.println(k);
+                    if (k == 33) break retry;
+                }
+            }
+
+        }
+
+
+    }
+}
--- Thread/src/ThreadPoolExecutorTrial.java
+++ Thread/src/ThreadPoolExecutorTrial.java
@@ -1,6 +1,5 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.Random;
+import java.util.concurrent.*;
 
 /**
  * Created by Administrator on 2018/10/26 9:56.
@@ -9,20 +8,89 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class ThreadPoolExecutorTrial {
     public static void main(String[] args) {
         ThreadPoolExecutorTrial threadPoolExecutorTrial = new ThreadPoolExecutorTrial();
-        threadPoolExecutorTrial.base();
+//        threadPoolExecutorTrial.base();
+        threadPoolExecutorTrial.custom();
+
+
+    }
+
+    private void custom() {
+
+        Runnable runnable = new Runnable() {
+            int i = new Random().nextInt(10);
+            @Override
+            public void run() {
+                System.out.println("one");
+                try {
+                    Thread.sleep(1000l);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        BaseThreadPoolExecutor baseThreadPoolExecutor =
+                new BaseThreadPoolExecutor(1, 1, 0L,
+                        TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+
+
+        baseThreadPoolExecutor.execute(runnable);
+        System.out.println(1);
+        baseThreadPoolExecutor.execute(runnable);
+        System.out.println(2);
+        baseThreadPoolExecutor.execute(runnable);
+        System.out.println(3);
+        baseThreadPoolExecutor.execute(runnable);
+        System.out.println(4);
+
+        baseThreadPoolExecutor.shutdown();
+
+
     }
 
     private void base() {
 
         Runnable runnable = new Runnable() {
+            int i = new Random().nextInt(10);
             @Override
             public void run() {
                 System.out.println("one");
+                try {
+                    Thread.sleep(2000l);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         };
 
         ExecutorService executorService = Executors.newSingleThreadExecutor();
-
         executorService.execute(runnable);
+        executorService.execute(runnable);
+        executorService.execute(runnable);
+        executorService.execute(runnable);
+        executorService.shutdown();
+    }
+}
+
+
+
+class BaseThreadPoolExecutor extends ThreadPoolExecutor{
+    public BaseThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    }
+
+    @Override
+    protected void beforeExecute(Thread t, Runnable r) {
+        System.out.println("~~beforeExecute~~");
+    }
+
+    @Override
+    protected void afterExecute(Runnable r, Throwable t) {
+        System.out.println("~~afterExecute~~");
+    }
+
+    @Override
+    protected void terminated() {
+        System.out.println("~~terminated~~");
     }
 }
