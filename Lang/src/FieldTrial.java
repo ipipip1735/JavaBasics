803d3ea8c43a737647d05b04c3411064b6607c92
ipipip1735 <ipipip1735@163.com>
2019-06-20 05:29:00 +0800

    Update Lang-reflect

--- Lang/src/FieldTrial.java
+++ Lang/src/FieldTrial.java
@@ -7,29 +7,30 @@ public class FieldTrial {
 
     public static void main(String[] args) {
         FieldTrial fieldTrial = new FieldTrial();
-        fieldTrial.noStatic();
-        fieldTrial.Static();
+        fieldTrial.staticField();
+//        fieldTrial.noStaticField();
     }
 
-    private void Static() {
+    private void noStaticField() {
 
         F ff = new F(1, false, "aaaaa");
         Field[] fields = F.class.getFields();
         for (Field f : fields) {
             try {
-                System.out.println(f.getName() + " = " + f.get(ff));//获取字段的值
+                System.out.println("NO-Static|" + f.getName() + " = " + f.get(ff));//获取字段的值
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
         }
+
     }
 
-    private void noStatic() {
+    private void staticField() {
 
         Field[] fields = FF.class.getFields();
         for (Field f : fields) {
             try {
-                System.out.println(f.getName() + " = " + f.get(null)); //获取静态字段的值
+                System.out.println("Static|" + f.getName() + " = " + f.get(null)); //获取静态字段的值
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
@@ -42,7 +43,9 @@ public class FieldTrial {
 class F {
     private int k;
     protected boolean bb;
-    public String ss;  //只有public能反射
+    public String ss = "ss";  //只有public能反射
+    static String tt = "tt";
+    public static String mm = "mm";
 
     public F(int k, boolean bb, String ss) {
         this.k = k;
@@ -54,5 +57,6 @@ class F {
 class FF {
     final private int k = 0;
     final protected boolean bb = true;
-    final static public String ss = "sadf";  //只有public能反射
+    final static public String ss = "ssssssss";  //只有public能反射
+//    public String tt = "tt";//无法读取非静态字段，抛异常
 }
