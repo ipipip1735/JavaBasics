74e7e52a6905a17841c0da1aa7b51b22ab00ceb4
ipipip1735 <ipipip1735@163.com>
2019-07-22 18:00:20 +0800

    Update Security

--- Security/src/KeyFactoryTrial.java
+++ Security/src/KeyFactoryTrial.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -59,13 +60,48 @@ public class KeyFactoryTrial {
                     .filter(s -> !s.contains("-"))
                     .collect(Collectors.joining());
             X509EncodedKeySpec pubPKCS8 = new X509EncodedKeySpec(Base64.getDecoder().decode(publicKey));
+            PublicKey pubKey = keyFactory.generatePublic(pubPKCS8);
 //            RSAPublicKeySpec rsaPublicKeySpec = new RSAPublicKeySpec(pubPKCS8)
 //            PublicKey pubKey = keyFactory.generatePublic(pubPKCS8);
+
+
+
+
+            Signature signature = Signature.getInstance("SHA256withRSA");
+            signature.initVerify(pubKey);
+
+            InputStream inputStream = Files.newInputStream(Paths.get("Security/res/message"));
+            byte[] buffer = new byte[1024];
+            int len;
+            while (inputStream.available() != 0) {
+                len = inputStream.read(buffer);
+                signature.update(buffer);
+                for (int i = 0; i < len; i++) {
+                    System.out.print(buffer[i] + ", ");
+                }
+                System.out.println("");
+            }
+            inputStream.close();
+
+
+            byte[] signData = Files.readAllBytes(Paths.get("Security/res/signData"));
+            boolean verifies = signature.verify(signData);
+            System.out.println("signature verifies: " + verifies);
+
+
+
+
 
 
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (SignatureException e) {
             e.printStackTrace();
         }
 
@@ -93,12 +129,16 @@ public class KeyFactoryTrial {
             PrivateKey priKey = keyFactory.generatePrivate(priPKCS8);
 
 
-            Signature sign = Signature.getInstance("SHA256withRSA");
-            sign.initSign(priKey);
-            sign.update(data.getBytes());
-            byte[] signature = sign.sign();
+            Signature signature = Signature.getInstance("SHA256withRSA");
+            signature.initSign(priKey);
+            signature.update(data.getBytes());
+            byte[] signData = signature.sign();
+            Files.write(Paths.get("Security/res/signData"), signData);
 
-            System.out.println(Base64.getEncoder().encodeToString(signature));
+            System.out.println(Base64.getEncoder().encodeToString(signData));
+
+
+
 
 
         } catch (IOException e) {
