2f12198521f7a575ec604bd26002e66e67d5d43b
ipipip1735 <toyota0626>
2019-07-31 08:07:09 +0800

    Update Security

--- Security/src/CertificateTrial.java
+++ Security/src/CertificateTrial.java
@@ -4,4 +4,18 @@
 
 public class CertificateTrial {
 
+    public static void main(String[] args) {
+        CertificateTrial certificateTrial = new CertificateTrial();
+
+        certificateTrial.creat();
+    }
+
+    private void creat() {
+
+        try (InputStream inStream = new FileInputStream("fileName-of-cert")) {
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            X509Certificate cert = (X509Certificate)cf.generateCertificate(inStream);
+        }
+    }
+
 }
