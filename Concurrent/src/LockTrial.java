330f4bea2be8ece7272228e407128dd96ed7e0ab
ipipip1735 <toyota0626>
2020-03-08 16:25:26 +0800

    Update Concurrent

--- Concurrent/src/LockTrial.java
+++ Concurrent/src/LockTrial.java
@@ -9,10 +9,88 @@ public class LockTrial {
     public static void main(String[] args) {
         LockTrial lockTrial = new LockTrial();
 //        lockTrial.lock();
-        lockTrial.condition();
+        lockTrial.condition();//一把锁反复使用
+        lockTrial.conditionTwo();//使用两把锁
     }
 
+
     private void condition() {
+
+        ReentrantLock reentrantLock = new ReentrantLock();
+        Condition condition = reentrantLock.newCondition();
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                reentrantLock.lock();//锁
+                System.out.println("sub lock");
+
+                System.out.println("signal main*******");
+                condition.signal();//唤醒主线程，否则主线程将一直等待，无论子线程是否结束
+
+                System.out.println("[sub]sleep1 is begin");
+                try {
+                    Thread.sleep(3000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("[sub]sleep1 is end");
+
+
+                try {
+                    condition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+                System.out.println("[sub]sleep2 is begin");
+                try {
+                    Thread.sleep(3000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("[sub]sleep2 is end");
+
+
+                reentrantLock.unlock();//解锁
+                System.out.println("sub unlock");
+            }
+        });
+
+
+        thread.start();
+
+        reentrantLock.lock(); //加锁
+        System.out.println("main Lock");
+        try {
+
+            System.out.println("main waiting------");
+            condition.await();//让出锁，子线程启动要时间，把锁让出来，子线程才能使用
+            System.out.println("main awake--------");
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("sleep is begin");
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("sleep is end");
+
+        condition.signal();
+        reentrantLock.unlock();//解锁
+        System.out.println("main unlock");
+
+
+    }
+
+    private void conditionTwo() {
 
         ReentrantLock reentrantLock = new ReentrantLock();
         Condition main = reentrantLock.newCondition();
