83de489ee9330b936c254bee6d2b2be4883b909c
ipipip1735 <ipipip1735@163.com>
2024-02-02 05:43:08 +0800

    update Base64

--- Security/src/Base64Trial.java
+++ Security/src/Base64Trial.java
@@ -1,7 +1,8 @@
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Arrays;
 import java.util.Base64;
 
 import static java.security.KeyFactory.getInstance;
@@ -13,10 +14,72 @@ import static java.security.KeyFactory.getInstance;
 public class Base64Trial {
     public static void main(String[] args) {
         Base64Trial base64Trial = new Base64Trial();
+        base64Trial.encoding(); //基本使用
 
-        base64Trial.encoding();
 
 
+//        base64Trial.decodeFile(new File("src/main/resources/onrar_encode"), new File("out_decode"));
+        base64Trial.encodeFile(new File("target/classes/onrar"), new File("target/classes/onrar_encode"));
+    }
+
+
+    private void encodeFile(File src, File des) {
+
+
+        try (FileInputStream fis = new FileInputStream(src);
+             BufferedInputStream bis = new BufferedInputStream(fis);
+             FileOutputStream fos = new FileOutputStream(des);
+             BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+            byte[] buffer = new byte[1024];
+            int length;
+            Base64.Encoder encoder = Base64.getEncoder();
+            while ((length = bis.read(buffer)) != -1) {
+                System.out.println("length = " + length);
+                if (length != 1024) buffer = Arrays.copyOf(buffer, length);
+
+                byte[] encode = encoder.encode(buffer);
+                System.out.println("encode.length = " + encode.length);
+                String s = new String(encode);
+                System.out.println("s = " + s);
+                bos.write(s.getBytes());
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void decodeFile(File src, File des) {
+        if (!src.exists()) {
+            System.out.println("src exists is false");
+            return;
+        }
+
+        try (FileInputStream fis = new FileInputStream(src);
+             BufferedInputStream bis = new BufferedInputStream(fis);
+             FileOutputStream fos = new FileOutputStream(new File("out_decode"));
+             BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+            byte[] buffer = new byte[1368];
+            int length;
+            Base64.Decoder decoder = Base64.getDecoder();
+            while ((length = bis.read(buffer)) != -1) {
+                System.out.println("length = " + length);
+                if (length != 1368) buffer = Arrays.copyOf(buffer, length);
+
+                byte[] decode = decoder.decode(buffer);
+                System.out.println("decode.length = " + decode.length);
+//                String s = new String(decode);
+//                System.out.println("s = " + s);
+                bos.write(decode);
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void encoding() {
